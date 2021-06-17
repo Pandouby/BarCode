e06db7a10740e58ff6e8872b409535838258b99b
YvesHuber <cpt.rino@gmail.com>
2021-06-17 15:45:53 +0200

    decode

--- Barcodewebapp/src/main/java/ch/bbw/Barcodewebapp/JSONwriter.java
+++ Barcodewebapp/src/main/java/ch/bbw/Barcodewebapp/JSONwriter.java
@@ -2,6 +2,7 @@ package ch.bbw.Barcodewebapp;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -9,16 +10,26 @@ import java.text.SimpleDateFormat;
 public class JSONwriter {
 
     public  void createFileIfNotExits(User user){
-        String yourFile = "Barcodewebapp/src/main/resources/Timestamps/"+user.getFirstname()+user.getFirstname()+".txt";
+        String yourFile = "Barcodewebapp/src/main/resources/Timestamps/"+user.getFirstname()+user.getLastname()+".txt";
         File tmpDir = new File(yourFile);
         boolean exists = tmpDir.exists();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String strDate = dateFormat.format(user.getLastLoggedIn());
         try {
 
             if (!exists) {
                 FileOutputStream fos = new FileOutputStream(yourFile);
-                fos.flush();
-                fos.close();
+
             }
+            try {
+                FileWriter myWriter = new FileWriter("Barcodewebapp/src/main/resources/Timestamps/"+user.getFirstname()+user.getLastname()+".txt");
+                myWriter.write(strDate + "\n");
+                myWriter.close();
+            } catch (IOException e) {
+
+            }
+
+
         } catch (Exception e){
 
         }
