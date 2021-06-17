10b609db2bbaab53a55976a84bf059b6514347c0
Pandouby <57749377+Pandouby@users.noreply.github.com>
2021-06-17 14:38:56 +0200

    Added constructor to User, Fixed Controller

--- Barcodewebapp/src/main/java/ch/bbw/Barcodewebapp/Controller.java
+++ Barcodewebapp/src/main/java/ch/bbw/Barcodewebapp/Controller.java
@@ -2,25 +2,23 @@ package ch.bbw.Barcodewebapp;
 
 public class Controller {
 
-    public String[] splitter(String code){
-        String[] values = new String[6];
-        values = code.split(".");
-        return values;
-    }
+    public User decode(String code) {
+        String[] values = code.split("\\.", -1);
+        User user = new User();
 
-    public String merger(String[] values){
-        String code = "";
-        code += values[0] + ".";
-        code += values[1] + ".";
-        code += values[2] + ".";
-        code += values[3] + ".";
-        return code;
-    }
-
-    public void setValuesOfUser(User user, String[] values){
         user.setFirstname(values[0]);
         user.setLastname(values[1]);
-        user.setClearance(Integer.parseInt(values[2]));
-        user.setDepartment(Integer.parseInt(values[3]));
+        user.setDepartment(Integer.parseInt(values[2]));
+        user.setClearance(Integer.parseInt(values[3]));
+        return user;
+    }
+
+    public String encode(User user) {
+        String code = "";
+        code += user.getFirstname() + ".";
+        code += user.getLastname() + ".";
+        code += user.getDepartment() + ".";
+        code += user.getClearance();
+        return code;
     }
 }
